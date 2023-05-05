2c3ed19cbb6008fbdcf67c7c38cbaad2c8127bb4
Srinivasan <n.m.srinivasan@accenture.com>
2023-05-05 15:39:23 +0530

    Updated my code

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 const fs = require('fs');
 const path = require('path');
-
 const express = require('express');
-const app = new express();
 
-app.set('views',path.join(__dirname, '/views'));
-app.set('view engine','ejs');
+const app = express();
 
-app.use(express.static(path.join(__dirname, '/public')));
+const { accounts, users, writeJSON } = require('./data');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 
-app.get('/', (req, res) => res.render('index', { title: 'Index' }));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.listen(3000, () => { console.log('PS Project Running on port 3000!') });
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.render('index', { title: 'Account Summary', accounts: accounts }));
+
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
+
+app.get('/profile', (req, res) =>  res.render('profile', { user: users[0] }));
+app.listen(3000, () => console.log('PS project running on port 3000'));
